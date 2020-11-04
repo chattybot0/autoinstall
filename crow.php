4c1b1351e19dd2535b40c2c84546aea1474d5485
chattybot0 <70719004+chattybot0@users.noreply.github.com>
2020-11-04 16:15:10 +0330

    Added an Auto-Updater

--- crow.php
+++ crow.php
@@ -1,14 +1,22 @@
 <?php
 
-function log($text = "No input specified, This means this must be a Crow.PHP error."){
+//The Auto-Updater, Planned to be moved into a function.
+$write = fopen("crow.php","w");
+$data = file_get_contents("https://raw.githubusercontent.com/chattybot0/autoinstall/main/crow.php");
+fwrite($write,$data);
+fclose($write);
+
+//Log a value to console
+function log($text = "No input specified, This means this must be a Crow.PHP FATAL error."){
   echo "<!--Added by Crow.php--><script>console.log(" . $text . ");</script>";
 }
 
+//Check for `ai.pkg`
 if(file_exists("ai.pkg")){
   
 }
 else {
-  log("Crow.PHP Fatal error: No ai.pkg file found, Or the permissions to read it is denied.");
+  log("Crow.PHP Load error: No ai.pkg file found, Or the permissions to read it is denied.");
 }
 
 ?>
