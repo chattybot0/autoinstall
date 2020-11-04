e22b42478c3588b663655d94c6ef683359d31665
chattybot0 <70719004+chattybot0@users.noreply.github.com>
2020-11-04 16:08:23 +0330

    Update crow.php

--- crow.php
+++ crow.php
@@ -1,5 +1,14 @@
 <?php
 
-echo "";
+function log($text = "No input specified, This means this must be a Crow.PHP error."){
+  echo "<!--Added by Crow.php--><script>console.log(" . $text . ");</script>";
+}
+
+if(file_exists("ai.pkg")){
+  
+}
+else {
+  log("Crow.PHP Fatal error: No ai.pkg file found, Or the permissions to read it is denied.");
+}
 
 ?>
