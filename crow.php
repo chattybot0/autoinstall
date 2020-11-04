83012544c8d0fc058cd7419bffc058f08ddf5676
Martia Foroud <70719004+chattybot0@users.noreply.github.com>
2020-11-04 23:25:13 +0330

    Finished the Installer, I hope.

--- crow.php
+++ crow.php
@@ -13,7 +13,7 @@ update();
 
 //Log a value to console
 function console_log($text = "No input specified, This means this must be a Crow.PHP FATAL error."){
-  echo "<!--Added by Crow.php--><script>console.log('" . $text . "');</script>";
+  echo "<script>console.log('" . $text . "');</script>";
 }
 
 function loadpkgs()
@@ -21,8 +21,14 @@ function loadpkgs()
   $read = fopen("ai.pkg","r");
   $urls = explode("\n",explode("packs@",fread($read,filesize("ai.pkg")))[1]);
   foreach($urls as $url){
-		$pkg = file_get_contents($url);
-		
+	  $pkg = file_get_contents($url);
+		$packs = explode("main@",$main)[1];
+    $main = explode("sep;\ndeps@\n",$packs)[0];
+    $deps = explode("\n",explode("sep;\ndeps@\n",$packs)[1]);
+    include($main);
+    foreach($deps as $dep){
+      include($dep);
+    }
   }
 }
 
